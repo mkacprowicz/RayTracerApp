0a3aa7677bca6466895af41ebc3db7ca72f26d57
Marcin <210211@edu.p.lodz.pl>
2020-03-24 20:07:24 +0100

    ImageFix

--- RayTracerApp/RayTracerApp/Image.cpp
+++ RayTracerApp/RayTracerApp/Image.cpp
@@ -1,5 +1,9 @@
 #include "Image.hpp"
 
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+
+#include "vendor/stb_image/stb_image.h"
 #include "vendor/stb_image/stb_image_write.h"
 
 Image::Image(int h, int w, int s)
